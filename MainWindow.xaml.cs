3da01c9e02054b27826f1180ea5f4b42a498156d
Vlad <vladane2033@gmail.com>
2020-12-21 22:25:05 +0200

    animation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using TRPZ.Models;
 
 
@@ -16,20 +18,28 @@ namespace TRPZ
         {
             InitializeComponent();
             db = new DatebaseAppContext();
-                db.Products.Add(new Product("Milk", 36));
-                db.Products.Add(new Product("Cheese", 50));
-                db.Products.Add(new Product("CottageCheese", 36));
-                db.Products.Add(new Product("Potato", 18));
-                db.Products.Add(new Product("Tomato", 42));
-                db.Products.Add(new Product("Strawberry", 120));
-                db.SaveChanges();
+            db.Products.Add(new Product("Milk", 36));
+            db.Products.Add(new Product("Cheese", 50));
+            db.Products.Add(new Product("CottageCheese", 36));
+            db.Products.Add(new Product("Potato", 18));
+            db.Products.Add(new Product("Tomato", 42));
+            db.Products.Add(new Product("Strawberry", 120));
+            db.SaveChanges();
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 400;
+            animation.Duration = TimeSpan.FromSeconds(1);
+            btnLogin.BeginAnimation(Button.WidthProperty, animation);
+            btnPassword.BeginAnimation(Button.WidthProperty, animation);
+
         }
 
         private void Button_Click_Login(object sender, RoutedEventArgs e)
         {
-           LoginWindow lw = new LoginWindow();
-           lw.Show();
-           this.Hide();
+            LoginWindow lw = new LoginWindow();
+            lw.Show();
+            this.Hide();
         }
 
         private void Button_Click_Register(object sender, RoutedEventArgs e)
